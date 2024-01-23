9102ceba7fc9922b60a34d470ac395881253c531
Bartosz_Kawa <bartekkawa2@gmail.com>
2024-01-23 16:41:00 +0100

    First changes

---
+++ source.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class Source:
+    Pages = {'theprotocol': 'https://theprotocol.it/',
+             'pracuj.pl': 'https://pracuj.pl',
+             'indeed': 'https://pl.indeed.com/'}
