a683e78eeeef01ab44f247c1f58de4fc728d76dc
Bartosz_Kawa <bartekkawa2@gmail.com>
2024-02-26 20:05:41 +0100

    Header complete for now

--- JobApp/react_app/src/components/Header.js
+++ JobApp/react_app/src/components/Header.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
+import logo from './logoHD.png'
+
 
 class Header extends Component{
+    headerStyle = {
+        backgroundColor: '#333', // Replace with your preferred color code
+        color: '#fff', // This sets the text color to white
+        padding: '10px',
+        textAlign: 'left'
+    };
+
+    availableWidth = window.screen.availWidth;
+    availableHeight = window.screen.availHeight;
     render() {
         return (
-            <div>
-                <img
-                    src="https://looka.com/editor/172565811"
-                    width="500"
-                    className="img-thumbnail"
-                    style={{ marginTop: "20px"}}
-                />
-                <hr />
-                <h1> Jobs Offers from some popular websites </h1>
-            </div>
+            <header style={this.headerStyle}>
+                <div style={{alignItems: "center"}}>
+                    <div className="image-container">
+                        <img src={logo} alt="logo" alt="Round Image" width={this.availableWidth / 4} height={this.availableHeight / 6}
+                             style={{margin: "20px", marginTop: "0px", borderRadius: '50%'}}/>
+                        <h1 style={{margin: "20px", marginTop: "20px"}}> Jobs Offers from some popular websites </h1>
+                        <hr/>
+                    </div>
+                </div>
+            </header>
         );
     }
 }
